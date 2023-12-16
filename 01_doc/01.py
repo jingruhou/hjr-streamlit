3cbc0c320d7bf690783ded02dad5c3eb4e64d5af
houjingru <houjingru07@gmail.com>
2023-12-16 17:04:45 +0800

    01-开发第一个Streamlit的Demo

---
+++ 01_doc/01.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+    @author: houjingru
+    @software: PyCharm
+    @file: 01.py
+    @Description: 
+    @time: 2023/12/16 16:58
+"""
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("这是一个Streamlit的Demo！")
